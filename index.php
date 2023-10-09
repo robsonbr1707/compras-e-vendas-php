bd22c8f071edc6d74ed1502ee21d939edd703cf2
Robson Luiz <75185287+robsonbr1707@users.noreply.github.com>
2023-10-09 09:16:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+      body{
+        max-width: 1980px;
+        font-family: 'Roboto Condensed', sans-serif;
+      }
+      .bg-dark{ background-color: #0a0a0a; }
+      .bg-dark2{ background-color: #111010; }
+      .bg-dark3{ background-color: #141313; }
+      .bg-body-modal{ background-color:rgba(12, 12, 12, 0.925); }
+    </style>
+    <title>Compras e vendas</title>
+    <script src="/assets/js/jquery/jquery-3-6-3.js"></script>
+    <script src="/assets/js/jquery/jquery-ajax-3-6-3.js"></script>
+    <script src="/assets/js/jquery/jqvalidate-1-19-5.js"></script>
+    <script src="/assets/js/jquery/jqvalidate-methods-1-19-5.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
